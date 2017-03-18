49711157cc692b21d8e1a146620b9d2cb78afe9c
Dmitriy Abramov <karvozavr@gmail.com>
2017-03-18 20:07:01 +0000

    done copy_exe.sh

--- hw_2/copy_exe.sh
+++ hw_2/copy_exe.sh
@@ -5,24 +5,20 @@ key2=$3
 indir=$2 
 outdir=$4
 
-if [[$1 == "-h" || $1 == "--help"]]
+if [[ "$key1" == "-o" && "$key2" == "-i" ]]
 	then
-		echo "Usage: ./copy_exe -i \"src_dir\" -o \"dst_dir\""
-fi
-
-if [[key1 == "-o" && $key2 == "-i"]]
-	then
-		outdir=$2
-		indir=$4
+		outdir="$2"
+		indir="$4"
 	else
-		if [[key1 == "-i" && $key2 == "-o"]]
-			outdir=$4
-			indir=$2
-		else
-			echo "Error: invalid argiments. Use \"./copy_exe.sh -h\" or \"./copy_exe.sh --help\" to see \"help\" information."
-			return 1
+		if [[ "$key1" == "-i" && "$key2" == "-o" ]]
+			then
+				outdir="$4"
+				indir="$2"
+			else
+				echo "Error: invalid argiments."
+				exit 1
 		fi
 fi
 
-find $indir -type f -executable -print -exec cp {} $outdir
+find "$indir" -type f -executable -exec cp {} "$outdir" > /dev/null 2> /dev/null \;
 
